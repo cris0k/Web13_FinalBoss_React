7646448d50402d8bd24b76ce9b72fc2702e43860
Cristina Kukhar <criskukhar@hotmail.com>
2023-02-08 21:12:15 +0100

    components folder

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom'
-import Header from '../layout/Header'
+import Header from './Header'
 
 const Layout = ({ children, title, ...props}) => <div>
     <Header {...props}/>
