6591c3aecadc055b0d520524eb9a92c653fe63d9
Juan Toledo <>
2023-03-05 14:27:31 +0100

    Placeholders translated

--- src/components/auth/RegisterPage.js
+++ src/components/auth/RegisterPage.js
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react"
-import { useForm } from "react-hook-form"
-import { useDispatch, useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import { registerUser} from "../../store/actions/authActions"
-import Error from "../Error"
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { registerUser } from "../../store/actions/authActions";
+import Error from "../Error";
 
 const RegisterPage = () => {
   const [customError, setCustomError] = useState(null);
+  const [t, i18n] = useTranslation("translation");
 
-  const { loading, error, token } = useSelector(
-    (state) => state.auth
-  );
+  const { loading, error, token } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   const { register, handleSubmit } = useForm();
@@ -18,67 +18,66 @@ const RegisterPage = () => {
 
   useEffect(() => {
     if (token) {
-      navigate('/')
+      navigate("/");
     }
-  }, [navigate, token])
+  }, [navigate, token]);
 
   const submitForm = (data) => {
-      if (data.password !== data.confirmPassword) {
-        setCustomError("Password mismatch");
-        return;
-      }
-      data.email = data.email.toLowerCase();
+    if (data.password !== data.confirmPassword) {
+      setCustomError("Password mismatch");
+      return;
+    }
+    data.email = data.email.toLowerCase();
 
-      dispatch(registerUser(data));
-
+    dispatch(registerUser(data));
   };
 
   return (
-    <form onSubmit={handleSubmit(submitForm)} className='signin-up-form'>
-      <h1 className='form-title'> Sign up </h1>
-      <div className='form-group'>
+    <form onSubmit={handleSubmit(submitForm)} className="signin-up-form">
+      <h1 className="form-title"> {t("Sign up")} </h1>
+      <div className="form-group">
         <input
-          type='text'
-          className='form-input'
-          placeholder='Name'
-          {...register('name')}
+          type="text"
+          className="form-input"
+          placeholder={t("Name")}
+          {...register("name")}
           required
         />
       </div>
-      <div className='form-group'>
+      <div className="form-group">
         <input
-          type='email'
-          className='form-input'
-          placeholder='Email'
-          {...register('email')}
+          type="email"
+          className="form-input"
+          placeholder={t("Email")}
+          {...register("email")}
           required
         />
       </div>
-      <div className='form-group'>
+      <div className="form-group">
         <input
-          type='password'
-          className='form-input'
-          placeholder='Password'
-          {...register('password')}
+          type="password"
+          className="form-input"
+          placeholder={t("Password")}
+          {...register("password")}
           required
         />
       </div>
-      <div className='form-group'>
+      <div className="form-group">
         <input
-          type='password'
-          className='form-input'
-          placeholder='Confirm Password'
-          {...register('confirmPassword')}
+          type="password"
+          className="form-input"
+          placeholder={t("Confirm password")}
+          {...register("confirmPassword")}
           required
         />
       </div>
       {error && <Error>{error}</Error>}
       {customError && <Error>{customError}</Error>}
-      <button type='submit' className='button' disabled={loading}>
-        {loading ? 'Loading...' : 'Register'}
+      <button type="submit" className="button" disabled={loading}>
+        {loading ? "Loading..." : "Register"}
       </button>
     </form>
-  )
-}
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
