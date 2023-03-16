cecaa6b99d772cb150899cf1ee63754f6c0f1b51
OrubieraR <107748119+OrubieraR@users.noreply.github.com>
2023-03-16 20:12:13 +0100

    Fixed send form without photo. Collected data with useform.

--- src/components/adverts/NewAdvert.js
+++ src/components/adverts/NewAdvert.js
@@ -1,18 +1,21 @@
 // Imports goes here
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useLocation, useParams, useNavigate } from 'react-router-dom';
 import Page from '../layout/Page';
 import { setAd } from '../auth/service';
 import { useForm } from 'react-hook-form';
+import { newAdvertSlice } from '../../store/slices/newAdvert';
 import i18n from '../../i18n';
 import { useTranslation } from 'react-i18next';
+
 import '../../style/form.css';
 
 const NewAdvert = (props) => {
 	const [t, i18n] = useTranslation('translation');
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
+
 	// const [name, setName] = useState("");
 	// const [sale, setSale] = useState("");
 	// let [tags, setTags] = useState("");
@@ -48,28 +51,71 @@ const NewAdvert = (props) => {
 	//   );
 	// };
 
+	const stateAdvertData = useSelector((state) => state.newAdvert);
+	// console.log(stateAdvertData);
+
+	const [formAdvert, setFormAdvert] = useState(stateAdvertData);
+
+	const handleInputChange = (e) => {
+		const category = document.querySelector('#category');
+		// console.log(category.options.selectedIndex);
+		// console.log(category);
+
+		// for (let i = 0; i < category.options.length; i++) {
+		// 	if (category.options.selectedIndex === i) {
+		// 		setFormAdvert({
+		// 			...formAdvert,
+		// 			[e.target.name]: e.target.value,
+		// 		});
+		// 	}
+		// }
+
+		// setFormAdvert({
+		// 	...formAdvert,
+		// 	[e.target.name]: e.target.value,
+		// });
+
+		// console.log(formAdvert);
+	};
+
 	// Useform managing destructuring
 	const { register, handleSubmit } = useForm();
 
+	// Example using useform to collect all data. Fails when data was submited.
+	// const onSubmit = (data) => {
+	// 	console.log(data);
+	// 	setAd(data);
+	// };
+
+	// It works with formData.
 	const onSubmit = (data) => {
 		const files = document.getElementById('photo');
 		// console.log(files);
 		const formData = new FormData();
-		formData.append('photo', data.photo[0].name);
-		formData.append('category', data.category.types);
+		// formData.append('photo', data.photo[0].name);
+		formData.append('category', data.category);
 		formData.append('name', data.name);
 		formData.append('price', data.price);
 		formData.append('userOwner', data.userOwner);
 		formData.append('company', data.company);
 		formData.append('sale', data.sale);
-		formData.append('PGI', data.pgi);
-		formData.append('description', data.description)
+		formData.append('PGI', data.PGI);
+		formData.append('description', data.description);
 
+		// formData.append('name', formAdvert.name);
+		// formData.append('company', formAdvert.company);
+		// formData.append('PGI', formAdvert.PGI);
+		// formData.append('sale', formAdvert.sale);
+		// formData.append('price', formAdvert.price);
+		// formData.append('photo', formAdvert.photo[0].name);
+		// formData.append('category', formAdvert.category);
+		// formData.append('description', formAdvert.description);
+		// console.log(register.name);
 		for (let i = 0; i < files.files.length; i++) {
 			formData.append('photo', files.files[i]);
 		}
 
-		console.log(data);
+		// console.log(data);
 		setAd(formData);
 	};
 
@@ -80,19 +126,20 @@ const NewAdvert = (props) => {
 				<form onSubmit={handleSubmit(onSubmit)}>
 					<br></br>
 					<label>Name:</label>
-
 					<input
 						id='name'
 						type='text'
 						placeholder='Write your name'
-						{...register('name', { required: true })}></input>
+						{...register('name', { required: true })}
+						onChange={handleInputChange}></input>
 					<br></br>
 					<label>User:</label>
 
 					<input
 						type='text'
 						placeholder='User name'
-						{...register('userOwner', { required: true })}></input>
+						{...register('userOwner', { required: true })}
+						onChange={handleInputChange}></input>
 
 					<br></br>
 					<label>Company:</label>
@@ -100,25 +147,34 @@ const NewAdvert = (props) => {
 					<input
 						type='text'
 						placeholder='Write your company name'
-						{...register('company', { required: true })}></input>
+						{...register('company', { required: true })}
+						onChange={handleInputChange}></input>
 
 					<br></br>
 					<br></br>
 					<h3>¿Is for buy or sale?</h3>
 					<label>Buy:</label>
 
-					<input type='radio' value='buy' {...register('sale')}></input>
+					<input
+						type='radio'
+						value='buy'
+						{...register('sale')}
+						onChange={handleInputChange}></input>
 
 					<label>Sale:</label>
 
-					<input type='radio' value='sale' {...register('sale')}></input>
+					<input
+						type='radio'
+						value='sale'
+						{...register('sale')}
+						onChange={handleInputChange}></input>
 
 					<br></br>
 					<br></br>
 					<br></br>
 					<label>PEGI:</label>
 
-					<select {...register('pgi')}>
+					<select {...register('PGI')} onChange={handleInputChange}>
 						<option value='3'>3</option>
 						<option value='7'>7</option>
 						<option value='12'>12</option>
@@ -132,7 +188,8 @@ const NewAdvert = (props) => {
 					<input
 						type='number'
 						step='0.01'
-						{...register('price', { required: true })}></input>
+						{...register('price', { required: true })}
+						onChange={handleInputChange}></input>
 
 					<br></br>
 					<br></br>
@@ -146,6 +203,7 @@ const NewAdvert = (props) => {
 						type='file'
 						// accept="image/png, image/jpeg"
 						{...register('photo')}
+						onChange={handleInputChange}
 						// multiple
 					></input>
 
@@ -173,7 +231,11 @@ const NewAdvert = (props) => {
               <option value="arcade">Arcade</option>
             </select> */}
 
-					<select {...register('category.types')} multiple>
+					<select
+						{...register('category')}
+						multiple
+						onChange={handleInputChange}
+						id='category'>
 						<option value='fantasy'>Fantasy</option>
 						<option value='rpg'>RPG</option>
 						<option value='shooter'>Shooter</option>
@@ -190,6 +252,7 @@ const NewAdvert = (props) => {
 					<br></br>
 					<textarea
 						{...register('description')}
+						onChange={handleInputChange}
 						style={{
 							maxWidth: '100%',
 							width: '475px',
